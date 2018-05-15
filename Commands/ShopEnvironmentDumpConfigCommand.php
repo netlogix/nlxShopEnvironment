d5065ff1d10b392a2d2119464988785c2326f2b0
Matthias Alt <alt@solutiondrive.de>
2018-05-15 21:19:37 +0200

    Renamed command to be more precise. Added Option to be able to set a target location where the file should be moved after export. default location ist var/export/

--- Commands/ShopEnvironmentDumpConfigCommand.php
+++ Commands/ShopEnvironmentDumpConfigCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Shopware\Commands\ShopwareCommand;
 
-class ShopEnvironmentDumpCommand extends ShopwareCommand
+class ShopEnvironmentDumpConfigCommand extends ShopwareCommand
 {
     /** @var ConfigurationDumperInterface */
     private $configurationDumper;
@@ -16,7 +16,7 @@ class ShopEnvironmentDumpCommand extends ShopwareCommand
     private $exportPath = '';
 
     /**
-     * ShopEnvironmentDumpCommand constructor.
+     * ShopEnvironmentDumpConfigCommand constructor.
      * @param ConfigurationDumperInterface $configurationDumper
      * @param string                       $defaultExportPath
      */
@@ -35,8 +35,9 @@ class ShopEnvironmentDumpCommand extends ShopwareCommand
     protected function configure()
     {
         $this
-            ->setName('sd:environment:dump')
+            ->setName('sd:environment:config:dump')
             ->addOption('filename', 'f', InputOption::VALUE_OPTIONAL, 'the name of the file where the configs should be exported to', 'shopware_configs.yaml')
+            ->addOption('target-directory', 't', InputOption::VALUE_OPTIONAL, 'the location where the exported file should be placed', 'default')
             ->setDescription('Dumps the current configs from the database to a yaml-File')
             ->setHelp(<<<EOF
 The <info>%command.name%</info> will dump all relevant config-values to a file.
@@ -50,6 +51,25 @@ EOF
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $filename = $input->getOption('filename');
+        $targetDirectory = $input->getOption('target-directory');
+
         $this->configurationDumper->dumpConfiguration($this->exportPath . '/' . $filename);
+
+        $output->writeln("<fg=yellow>Config values from `s_core_config_elements` were exported to <fg=green>$this->exportPath/$filename</> succesfully</>");
+
+        if ('default' !== $targetDirectory) {
+            $output->writeln('moving file to '.$targetDirectory);
+            if (false === is_writable($targetDirectory)) {
+                $output->writeln("directory does not exist: <fg=red>$targetDirectory</>");
+                exit(1);
+            }
+            if (rename($this->exportPath . '/' . $filename, $targetDirectory . '/' . $filename)) {
+               $output->writeln('<fg=green>file successfully moved</>');
+               exit(0);
+            }
+            $output->writeln('<fg=red>moving file to '.$targetDirectory.' failed!</>');
+            exit(1);
+        }
+        exit(0);
     }
 }
