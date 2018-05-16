984bd93719a563c7e7be40972f9dbfcf8810da04
Matthias Alt <alt@solutiondrive.de>
2018-05-16 13:54:29 +0200

    added logic for loading/creating forms and elements if they do not exist

--- Services/ConfigurationLoader.php
+++ Services/ConfigurationLoader.php
@@ -11,6 +11,7 @@ namespace sdShopEnvironment\Services;
 use Shopware\Components\DependencyInjection\Container;
 use Shopware\Components\Model\ModelManager;
 use Shopware\Models\Config\Element;
+use Shopware\Models\Config\Form;
 use Symfony\Component\Yaml\Yaml;
 
 class ConfigurationLoader implements ConfigurationLoaderInterface
@@ -35,23 +36,58 @@ class ConfigurationLoader implements ConfigurationLoaderInterface
         /** @var ModelManager $entityManager */
         $entityManager = $this->container->get('models');
         $configElementRepository = $entityManager->getRepository('Shopware\Models\Config\Element');
+        $configFormRepository = $entityManager->getRepository('Shopware\Models\Config\Form');
 
         $contentOfYamlFile = Yaml::parse($pathToFile);
 
         // @todo continue here when all data is dumped correctly
-        /*foreach ($contentOfYamlFile as $nameOfBackendForm => $formElements) {
-            foreach ($formElements as $elementName => $elementValue) {
-                $element = $configElementRepository->findOneBy(['name' => $elementName]);
-                if (null === $element) {
-                    if (is_int($elementValue)) {
+        foreach ($contentOfYamlFile as $nameOfBackendForm => $formElements) {
+            foreach ($formElements as $elementName => $elementInformation) {
 
-                    } elseif( is_array($elementValue)) {
+                $element = $this->findOrCreateElement($configElementRepository, $elementName, $elementInformation);
+                $form = $this->findOrCreateForm($configFormRepository, $elementInformation);
 
-                    }
-                }
+                // update existing element
+                $element->setForm($form);
+                $element->setLabel($elementInformation['label']);
+                $element->setDescription($elementInformation['description']);
+                $element->setPosition($elementInformation['position']);
+                $element->setScope($elementInformation['scope']);
+                $element->setValue($elementInformation['value']);
+                $entityManager->persist($element);
             }
-        }*/
+        }
+        $entityManager->flush();
+    }
 
+    /**
+     * @param $configFormRepository
+     * @param $elementInformation
+     * @return Form
+     */
+    private function findOrCreateForm($configFormRepository, $elementInformation)
+    {
+        $form = $configFormRepository->findOneBy(['name' => $elementInformation['form']['name']]);
+        if (null === $form) {
+            $form = new Form();
+        }
 
+        $form->setName($elementInformation['form']['name']);
+        $form->setLabel($elementInformation['form']['label']);
+        $form->setDescription($elementInformation['form']['description']);
+        $form->setPosition($elementInformation['form']['position']);
+        return $form;
+    }
+
+    private function findOrCreateElement($configElementRepository, $elementName, $elementInformation)
+    {
+        $element = $configElementRepository->findOneBy(['name' => $elementName]);
+        if (null === $element) {
+            $elementType = $elementInformation['type'];
+            $elementOptions = $elementInformation['options'];
+
+            $element = new Element($elementType, $elementName, $elementOptions);
+        }
+        return $element;
     }
 }
