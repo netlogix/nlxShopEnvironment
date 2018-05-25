9190f951d0776c8ad21a529f0dcc69aa64d1a543
Michael Zapf <mz@solutiondrive.de>
2018-05-25 11:50:31 +0200

    add some informational output

--- etc/scripts/prepareComposerJson.sh
+++ etc/scripts/prepareComposerJson.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Preparing composer.json..."
+echo "PHP version:      "$(php -r "echo PHP_VERSION;")
+echo "Shopware version: "$SHOPWARE_VERSION
+echo "Composer version: "$(composer -V)
+
 sed -i "s~[\"]shopware/shopware[\"]: [\"]^5.2[\"]~\"shopware/shopware\": \"${SHOPWARE_VERSION}\"~g" composer.json
