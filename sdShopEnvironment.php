c7485466ca76356af681ed88fadedb34b615286d
Michael Zapf <mz@solutiondrive.de>
2018-08-15 10:30:34 +0200

    be more helpful in comment

--- sdShopEnvironment.php
+++ sdShopEnvironment.php
@@ -8,6 +8,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 // We know, this makes problems in development environment (if Shopware is installed into vendor),
 // but YAML component must be loaded here if this plugin is not installed via composer.
+// To reduce errors in developement you can simply delete the vendor folder or comment these lines (but don't commit!)
 if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
