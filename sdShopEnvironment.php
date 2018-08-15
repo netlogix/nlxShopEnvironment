b123f47d37faa0af78f9e4f394b5135ff1f9859e
Michael Zapf <mz@solutiondrive.de>
2018-08-15 10:28:57 +0200

    load autloader from vendor

--- sdShopEnvironment.php
+++ sdShopEnvironment.php
@@ -6,6 +6,12 @@ use Shopware\Components\Plugin;
 use Shopware\Components\Console\Application;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
+// We know, this makes problems in development environment (if Shopware is installed into vendor),
+// but YAML component must be loaded here if this plugin is not installed via composer.
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
+
 /**
  * Shopware-Plugin sdShopEnvironment.
  */
