1fe7c3547b63d9986c781bd4ab4e68bbb8949d80
Tobias Wojtylak <tw@solutiondrive.de>
2019-01-16 17:07:59 +0100

    INFRA-74 Implemented PaymentMethodsLoader

--- Loader/PaymentMethodsLoader.php
+++ Loader/PaymentMethodsLoader.php
@@ -8,13 +8,60 @@
 
 namespace sdShopEnvironment\Loader;
 
+use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Shopware\Models\Payment\Payment;
+use Shopware\Models\Shop\Shop;
+
 class PaymentMethodsLoader implements LoaderInterface
 {
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    /** @var ObjectRepository */
+    private $paymentMethodsRepository;
+
+    /** @var ObjectRepository */
+    private $shopRepository;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->paymentMethodsRepository = $entityManager->getRepository(Payment::class);
+        $this->shopRepository = $entityManager->getRepository(Shop::class);
+    }
+
     /**
      * {@inheritdoc}
      */
     public function load($config)
     {
-        // TODO: Implement load() method.
+        foreach ($config as $id => $paymentMethodData) {
+            try {
+                $this->importPaymentMethod($id, $paymentMethodData);
+            } catch (\Throwable $throwable) {
+                echo 'Error during import of payment method ' . $id . PHP_EOL;
+                echo $throwable->getMessage();
+            }
+        }
+
+        $this->entityManager->flush();
+    }
+
+    /**
+     * @param string $paymentMethodName
+     * @param array  $paymentMethodData
+     */
+    private function importPaymentMethod($paymentMethodName, $paymentMethodData)
+    {
+        $paymentMethod = $this->paymentMethodsRepository->findOneBy(['name' => $paymentMethodName]);
+        if (null === $paymentMethod) {
+            $paymentMethod = new Payment();
+            $paymentMethod->setName($paymentMethodName);
+            $this->entityManager->persist($paymentMethod);
+        }
+
+        $paymentMethodData['shops'] = $this->shopRepository->findBy(['id' => $paymentMethodData['shops']]);
+        $paymentMethod->fromArray($paymentMethodData);
     }
 }
--- spec/Loader/PaymentMethodsLoaderSpec.php
+++ spec/Loader/PaymentMethodsLoaderSpec.php
@@ -8,12 +8,37 @@
 
 namespace spec\sdShopEnvironment\Loader;
 
+use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 use sdShopEnvironment\Loader\LoaderInterface;
 use sdShopEnvironment\Loader\PaymentMethodsLoader;
+use Shopware\Models\Payment\Payment;
+use Shopware\Models\Shop\Shop;
 
 class PaymentMethodsLoaderSpec extends ObjectBehavior
 {
+    public function let(
+        EntityManagerInterface $entityManager,
+        ObjectRepository $paymentMethodsRepository,
+        ObjectRepository $shopRepository
+    ) {
+        $entityManager
+            ->getRepository(Payment::class)
+            ->willReturn($paymentMethodsRepository);
+
+        $shopRepository
+            ->findBy(Argument::any())
+            ->willReturn([]);
+
+        $entityManager
+            ->getRepository(Shop::class)
+            ->willReturn($shopRepository);
+
+        $this->beConstructedWith($entityManager);
+    }
+
     public function it_is_initializable()
     {
         $this->shouldHaveType(PaymentMethodsLoader::class);
@@ -22,5 +47,52 @@ class PaymentMethodsLoaderSpec extends ObjectBehavior
     public function it_implements_correct_interface()
     {
         $this->shouldImplement(LoaderInterface::class);
+    }
+
+    public function it_can_load_new_payment_methods(
+        EntityManagerInterface $entityManager,
+        ObjectRepository $paymentMethodsRepository
+    ) {
+        $paymentMethodsRepository
+            ->findOneBy(['name' => 'new_payment_method'])
+            ->willReturn(null);
+
+        $data = ['new_payment_method' => ['description' => 'hello world', 'shops' => []]];
+
+        $entityManager
+            ->persist(Argument::type(Payment::class))
+            ->shouldBeCalled();
+
+        $entityManager
+            ->flush()
+            ->shouldBeCalled();
+
+        $this->load($data);
+    }
+
+    public function it_can_load_existing_payment_methods(
+        EntityManagerInterface $entityManager,
+        ObjectRepository $paymentMethodsRepository,
+        Payment $paymentMethod
+    ) {
+        $paymentMethodsRepository
+            ->findOneBy(['name' => 'new_payment_method'])
+            ->willReturn($paymentMethod);
+
+        $data = ['new_payment_method' => ['description' => 'hello world', 'shops' => []]];
+
+        $entityManager
+            ->persist(Argument::any())
+            ->shouldNotBeCalled();
+
+        $paymentMethod
+            ->fromArray($data['new_payment_method'])
+            ->shouldBeCalled();
+
+        $entityManager
+            ->flush()
+            ->shouldBeCalled();
+
+        $this->load($data);
     }
 }
