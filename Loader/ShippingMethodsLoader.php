4a475e34f680509bfb022833dedab64d694656aa
Tobias Wojtylak <tw@solutiondrive.de>
2019-01-17 14:41:40 +0100

    INFRA-89 Implement ShippingMethodsLoader

--- Loader/ShippingMethodsLoader.php
+++ Loader/ShippingMethodsLoader.php
@@ -8,13 +8,58 @@
 
 namespace sdShopEnvironment\Loader;
 
+use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Shopware\Models\Dispatch\Dispatch;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
 class ShippingMethodsLoader implements LoaderInterface
 {
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    /** @var ObjectRepository */
+    private $shippingMethodsRepository;
+
+    /** @var DenormalizerInterface */
+    private $denormalizer;
+
+    public function __construct(EntityManagerInterface $entityManager, DenormalizerInterface $denormalizer)
+    {
+        $this->entityManager = $entityManager;
+        $this->shippingMethodsRepository = $entityManager->getRepository(Dispatch::class);
+        $this->denormalizer = $denormalizer;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function load($config)
     {
-        // TODO: Implement load() method.
+        foreach ($config as $id => $shippingMethodData) {
+            try {
+                $this->importShippingMethod($id, $shippingMethodData);
+            } catch (\Throwable $throwable) {
+                echo 'Error during import of shipping method ' . $id . PHP_EOL;
+                echo $throwable->getMessage();
+            }
+        }
+
+        $this->entityManager->flush();
+    }
+
+    /**
+     * @param int   $shippingMethodId
+     * @param array $shippingMethodData
+     */
+    private function importShippingMethod($shippingMethodId, $shippingMethodData)
+    {
+        $shippingMethod = $this->shippingMethodsRepository->find($shippingMethodId);
+        if (null === $shippingMethod) {
+            $shippingMethod = new Dispatch();
+            $this->entityManager->persist($shippingMethod);
+        }
+
+        $this->denormalizer->denormalize($shippingMethodData, Dispatch::class, null, ['object_to_populate' => $shippingMethod]);
     }
 }
--- spec/Loader/ShippingMethodsLoaderSpec.php
+++ spec/Loader/ShippingMethodsLoaderSpec.php
@@ -8,12 +8,29 @@
 
 namespace spec\sdShopEnvironment\Loader;
 
+use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 use sdShopEnvironment\Loader\LoaderInterface;
 use sdShopEnvironment\Loader\ShippingMethodsLoader;
+use Shopware\Models\Dispatch\Dispatch;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 
 class ShippingMethodsLoaderSpec extends ObjectBehavior
 {
+    public function let(
+        EntityManagerInterface $entityManager,
+        ObjectRepository $shippingMethodsRepository,
+        DenormalizerInterface $denormalizer
+    ) {
+        $entityManager
+            ->getRepository(Dispatch::class)
+            ->willReturn($shippingMethodsRepository);
+
+        $this->beConstructedWith($entityManager, $denormalizer);
+    }
+
     public function it_is_initializable()
     {
         $this->shouldHaveType(ShippingMethodsLoader::class);
@@ -22,5 +39,63 @@ class ShippingMethodsLoaderSpec extends ObjectBehavior
     public function it_implements_correct_interface()
     {
         $this->shouldImplement(LoaderInterface::class);
+    }
+
+    public function it_can_load_new_shipping_methods(
+        EntityManagerInterface $entityManager,
+        ObjectRepository $shippingMethodsRepository,
+        DenormalizerInterface $denormalizer
+    ) {
+        $shippingMethodsRepository
+            ->find(13)
+            ->willReturn(null);
+
+        $data = [13 => ['description' => 'hello world']];
+
+        $denormalizer
+            ->denormalize(Argument::any(), Argument::any(), Argument::any(), Argument::any())
+            ->shouldBeCalled();
+
+        $entityManager
+            ->persist(Argument::type(Dispatch::class))
+            ->shouldBeCalled();
+
+        $entityManager
+            ->flush()
+            ->shouldBeCalled();
+
+        $this->load($data);
+    }
+
+    public function it_can_load_existing_shipping_methods(
+        EntityManagerInterface $entityManager,
+        ObjectRepository $shippingMethodsRepository,
+        Dispatch $shippingMethod,
+        DenormalizerInterface $denormalizer
+    ) {
+        $shippingMethodsRepository
+            ->find(12)
+            ->willReturn($shippingMethod);
+
+        $data = [12 => ['description' => 'hello world']];
+
+        $denormalizer
+            ->denormalize(
+                Argument::any(),
+                Argument::any(),
+                Argument::any(),
+                Argument::withEntry('object_to_populate', $shippingMethod->getWrappedObject())
+            )
+            ->shouldBeCalled();
+
+        $entityManager
+            ->persist(Argument::any())
+            ->shouldNotBeCalled();
+
+        $entityManager
+            ->flush()
+            ->shouldBeCalled();
+
+        $this->load($data);
     }
 }
