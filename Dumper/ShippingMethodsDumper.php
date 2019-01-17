82f98675475bcf481be146a89408fcbd2c7a114f
Tobias Wojtylak <tw@solutiondrive.de>
2019-01-17 14:32:51 +0100

    INFRA-89 Implement ShippingMethodsDumper

--- Dumper/ShippingMethodsDumper.php
+++ Dumper/ShippingMethodsDumper.php
@@ -8,13 +8,35 @@
 
 namespace sdShopEnvironment\Dumper;
 
+use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Shopware\Models\Dispatch\Dispatch;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
 class ShippingMethodsDumper implements DumperInterface
 {
+    /** @var ObjectRepository */
+    private $shippingMethodsRepository;
+
+    /** @var NormalizerInterface */
+    private $normalizer;
+
+    public function __construct(EntityManagerInterface $entityManager, NormalizerInterface $normalizer)
+    {
+        $this->shippingMethodsRepository = $entityManager->getRepository(Dispatch::class);
+        $this->normalizer = $normalizer;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function dump()
     {
-        // TODO: Implement dump() method.
+        $shippingMethods = [];
+        foreach ($this->shippingMethodsRepository->findAll() as $paymentMethod) {
+            $shippingMethods[$paymentMethod->getId()] = $this->normalizer->normalize($paymentMethod);
+        }
+
+        return $shippingMethods;
     }
 }
--- spec/Dumper/ShippingMethodsDumperSpec.php
+++ spec/Dumper/ShippingMethodsDumperSpec.php
@@ -8,12 +8,29 @@
 
 namespace spec\sdShopEnvironment\Dumper;
 
+use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 use sdShopEnvironment\Dumper\DumperInterface;
 use sdShopEnvironment\Dumper\ShippingMethodsDumper;
+use Shopware\Models\Dispatch\Dispatch;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class ShippingMethodsDumperSpec extends ObjectBehavior
 {
+    public function let(
+        EntityManagerInterface $entityManager,
+        ObjectRepository $shippingMethodsRepository,
+        NormalizerInterface $normalizer
+    ) {
+        $entityManager
+            ->getRepository(Dispatch::class)
+            ->willReturn($shippingMethodsRepository);
+
+        $this->beConstructedWith($entityManager, $normalizer);
+    }
+
     public function it_is_initializable()
     {
         $this->shouldHaveType(ShippingMethodsDumper::class);
@@ -22,5 +39,49 @@ class ShippingMethodsDumperSpec extends ObjectBehavior
     public function it_implements_correct_interface()
     {
         $this->shouldImplement(DumperInterface::class);
+    }
+
+    public function it_can_dump_empty_shipping_methods(ObjectRepository $shippingMethodsRepository)
+    {
+        $shippingMethodsRepository
+            ->findAll()
+            ->shouldBeCalled()
+            ->willReturn([]);
+
+        $this
+            ->dump()
+            ->shouldBe([]);
+    }
+
+    public function it_can_dump_shipping_methods(
+        ObjectRepository $shippingMethodsRepository,
+        Dispatch $shippingMethodOne,
+        Dispatch $shippingMethodTwo,
+        NormalizerInterface $normalizer
+    ) {
+        $shippingMethodOne
+            ->getId()
+            ->willReturn(13);
+
+        $shippingMethodTwo
+            ->getId()
+            ->willReturn(12);
+
+        $shippingMethodsRepository
+            ->findAll()
+            ->shouldBeCalled()
+            ->willReturn([$shippingMethodOne, $shippingMethodTwo]);
+
+        $normalizer
+            ->normalize(Argument::type(Dispatch::class))
+            ->shouldBeCalledTimes(2)
+            ->willReturn(['data' => 'data']);
+
+        $this
+            ->dump()
+            ->shouldBeLike([
+                13 => ['data' => 'data'],
+                12 => ['data' => 'data'],
+            ]);
     }
 }
