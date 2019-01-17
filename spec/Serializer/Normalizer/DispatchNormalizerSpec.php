cea7cfaec4fda1bf3a3234120cf3ea12d77a06e8
Tobias Wojtylak <tw@solutiondrive.de>
2019-01-17 19:10:31 +0100

    INFRA-89 Added spec for DispatchNormalizer setAttributeValue

---
+++ spec/Serializer/Normalizer/DispatchNormalizerSpec.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * Created by solutionDrive GmbH
+ *
+ * @copyright 2018 solutionDrive GmbH
+ */
+
+namespace spec\sdShopEnvironment\Serializer\Normalizer;
+
+use PhpSpec\ObjectBehavior;
+use sdShopEnvironment\Serializer\Normalizer\DispatchNormalizer;
+use Shopware\Models\Country\Country;
+use Shopware\Models\Dispatch\Dispatch;
+use Shopware\Models\Dispatch\ShippingCost;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Serializer;
+
+class DispatchNormalizerSpec extends ObjectBehavior
+{
+    public function let(Serializer $serializer)
+    {
+        $this->setSerializer($serializer);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(DispatchNormalizer::class);
+    }
+
+    public function it_implements_correct_interfaces()
+    {
+        $this->shouldImplement(NormalizerInterface::class);
+        $this->shouldImplement(DenormalizerInterface::class);
+    }
+
+    public function it_supports_dispatch_denormalization()
+    {
+        $this->supportsDenormalization([], Dispatch::class)->shouldBe(true);
+    }
+
+    public function it_does_not_support_other_denormalization()
+    {
+        $this->supportsDenormalization([], \stdClass::class)->shouldBe(false);
+    }
+
+    public function it_will_call_serializer_on_countries_attribute(
+        \stdClass $object,
+        Serializer $serializer
+    ) {
+        $serializer
+            ->denormalize([1], Country::class .'[]', null)
+            ->shouldBeCalled()
+            ->willReturn([]);
+
+        $this->setAttributeValue($object, 'countries', [1]);
+    }
+
+    public function it_will_call_serializer_on_shops_attribute(
+        \stdClass $object,
+        Serializer $serializer
+    ) {
+        $serializer
+            ->denormalize([1], ShippingCost::class .'[]', null)
+            ->shouldBeCalled()
+            ->willReturn([]);
+
+        $this->setAttributeValue($object, 'costsMatrix', [1]);
+    }
+}
