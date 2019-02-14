fee9b0e7a933e1df30295037bbb307da6efbb0cf
Michael Zapf <mz@solutiondrive.de>
2019-02-14 16:09:07 +0100

    add specs for documents loader and dumper

---
+++ spec/Dumper/DocumentsDumperSpec.php
@@ -0,0 +1,126 @@
+<?php
+
+/*
+ * Created by solutionDrive GmbH
+ *
+ * @copyright solutionDrive GmbH
+ */
+
+namespace spec\sdShopEnvironment\Dumper;
+
+use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use sdShopEnvironment\Dumper\DocumentsDumper;
+use sdShopEnvironment\Dumper\DumperInterface;
+use Shopware\Models\Document\Document;
+use Shopware\Models\Payment\Payment;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class DocumentsDumperSpec extends ObjectBehavior
+{
+    public function let(
+        EntityManagerInterface $entityManager,
+        ObjectRepository $documentsRepository
+    ) {
+        $entityManager
+            ->getRepository(Document::class)
+            ->willReturn($documentsRepository);
+
+        $this->beConstructedWith($entityManager);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(DocumentsDumper::class);
+    }
+
+    public function it_implements_correct_interface()
+    {
+        $this->shouldImplement(DumperInterface::class);
+    }
+
+    public function it_can_dump_empty_documents(
+        ObjectRepository $documentsRepository
+    ) {
+        $documentsRepository->findAll()
+            ->shouldBeCalled()
+            ->willReturn([]);
+
+        $this->dump()
+            ->shouldBe([]);
+    }
+
+    public function it_can_dump_document(
+        ObjectRepository $documentsRepository,
+        Document $document1,
+        Document $document2
+    ) {
+        $document1->getKey()
+            ->willReturn('doc1');
+        $document1->getName()
+            ->willReturn('documentOne');
+        $document1->getTemplate()
+            ->willReturn('doc1.tpl');
+        $document1->getNumbers()
+            ->willReturn('doc01');
+        $document1->getLeft()
+            ->willReturn(99);
+        $document1->getRight()
+            ->willReturn(88);
+        $document1->getTop()
+            ->willReturn(77);
+        $document1->getBottom()
+            ->willReturn(66);
+        $document1->getPageBreak()
+            ->willReturn(55);
+
+        $document2->getKey()
+            ->willReturn('doc2');
+        $document2->getName()
+            ->willReturn('documentTwo');
+        $document2->getTemplate()
+            ->willReturn('doc2.tpl');
+        $document2->getNumbers()
+            ->willReturn('doc02');
+        $document2->getLeft()
+            ->willReturn(99);
+        $document2->getRight()
+            ->willReturn(88);
+        $document2->getTop()
+            ->willReturn(77);
+        $document2->getBottom()
+            ->willReturn(66);
+        $document2->getPageBreak()
+            ->willReturn(55);
+
+        $documentsRepository->findAll()
+            ->shouldBeCalled()
+            ->willReturn([$document1, $document2]);
+
+        $this->dump()
+            ->shouldBeLike([
+                'doc1' => [
+                    'name'      => 'documentOne',
+                    'template'  => 'doc1.tpl',
+                    'numbers'   => 'doc01',
+                    'left'      => 99,
+                    'right'     => 88,
+                    'top'       => 77,
+                    'bottom'    => 66,
+                    'pagebreak' => 55,
+                ],
+                'doc2' => [
+                    'name'      => 'documentTwo',
+                    'template'  => 'doc2.tpl',
+                    'numbers'   => 'doc02',
+                    'left'      => 99,
+                    'right'     => 88,
+                    'top'       => 77,
+                    'bottom'    => 66,
+                    'pagebreak' => 55,
+                ],
+            ]);
+    }
+}
---
+++ spec/Loader/DocumentsLoaderSpec.php
@@ -0,0 +1,146 @@
+<?php
+
+/*
+ * Created by solutionDrive GmbH
+ *
+ * @copyright solutionDrive GmbH
+ */
+
+namespace spec\sdShopEnvironment\Loader;
+
+use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use sdShopEnvironment\Loader\DocumentsLoader;
+use sdShopEnvironment\Loader\LoaderInterface;
+use Shopware\Models\Document\Document;
+use Webmozart\Assert\Assert;
+
+class DocumentsLoaderSpec extends ObjectBehavior
+{
+    public function let(
+        EntityManagerInterface $entityManager,
+        ObjectRepository $documentsRepository
+    ) {
+        $entityManager
+            ->getRepository(Document::class)
+            ->willReturn($documentsRepository);
+
+        $this->beConstructedWith($entityManager);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(DocumentsLoader::class);
+    }
+
+    public function it_implements_correct_interface()
+    {
+        $this->shouldImplement(LoaderInterface::class);
+    }
+
+    public function it_can_load_empty(
+        EntityManagerInterface $entityManager,
+        ObjectRepository $documentsRepository
+    ) {
+        $documentsRepository->findOneBy(Argument::any())
+            ->shouldNotBeCalled();
+
+        $entityManager->flush()
+            ->shouldBeCalled();
+
+        $this->load([]);
+    }
+
+    public function it_can_update_existing_document(
+        EntityManagerInterface $entityManager,
+        ObjectRepository $documentsRepository,
+        Document $document1
+    ) {
+        $data = [
+            'doc1' => [
+                'name'      => 'documentOne',
+                'template'  => 'doc1.tpl',
+                'numbers'   => 'doc01',
+                'left'      => 99,
+                'right'     => 88,
+                'top'       => 77,
+                'bottom'    => 66,
+                'pagebreak' => 55,
+            ],
+        ];
+
+        $documentsRepository->findOneBy(['key' => 'doc1'])
+            ->willReturn($document1);
+
+        $document1->setName('documentOne')
+            ->shouldBeCalled();
+        $document1->setTemplate('doc1.tpl')
+            ->shouldBeCalled();
+        $document1->setNumbers('doc01')
+            ->shouldBeCalled();
+        $document1->setLeft(99)
+            ->shouldBeCalled();
+        $document1->setRight(88)
+            ->shouldBeCalled();
+        $document1->setTop(77)
+            ->shouldBeCalled();
+        $document1->setBottom(66)
+            ->shouldBeCalled();
+        $document1->setPageBreak(55)
+            ->shouldBeCalled();
+
+        $entityManager->flush()
+            ->shouldBeCalled();
+
+        $this->load($data);
+    }
+
+    public function it_can_create_new_document(
+        EntityManagerInterface $entityManager,
+        ObjectRepository $documentsRepository,
+        Document $document1
+    ) {
+        $data = [
+            'doc1' => [
+                'name'      => 'documentOne',
+                'template'  => 'doc1.tpl',
+                'numbers'   => 'doc01',
+                'left'      => 99,
+                'right'     => 88,
+                'top'       => 77,
+                'bottom'    => 66,
+                'pagebreak' => 55,
+            ],
+        ];
+
+        $documentsRepository->findOneBy(['key' => 'doc1'])
+            ->willReturn(null);
+
+        $persistedDocument = null;
+        $entityManager->persist(Argument::that(function ($document) use (&$persistedDocument) {
+            if (false === is_a($document, Document::class)) {
+                return false;
+            }
+
+            $persistedDocument = $document;
+            return true;
+        }))
+            ->shouldBeCalled();
+        $entityManager->flush()
+            ->shouldBeCalled();
+
+        $this->load($data);
+
+        Assert::eq($persistedDocument->getName(), 'documentOne');
+        Assert::eq($persistedDocument->getKey(), 'doc1');
+        Assert::eq($persistedDocument->getTemplate(), 'doc1.tpl');
+        Assert::eq($persistedDocument->getNumbers(), 'doc01');
+        Assert::eq($persistedDocument->getLeft(), 99);
+        Assert::eq($persistedDocument->getRight(), 88);
+        Assert::eq($persistedDocument->getTop(), 77);
+        Assert::eq($persistedDocument->getBottom(), 66);
+        Assert::eq($persistedDocument->getPageBreak(), 55);
+    }
+}
