bcc715b6dc2ed1906d5e68e7b9e5f6c0e468e69f
Michael Zapf <mz@solutiondrive.de>
2019-02-15 08:42:52 +0100

    improve dump documents spec to not duplicate code

--- spec/Dumper/DocumentsDumperSpec.php
+++ spec/Dumper/DocumentsDumperSpec.php
@@ -58,47 +58,13 @@ class DocumentsDumperSpec extends ObjectBehavior
             return;
         }
 
+        $this->prepareParametersForDump($documentsRepository, $document1, $document2);
+
         $document1->getKey()
             ->willReturn('doc1');
-        $document1->getName()
-            ->willReturn('documentOne');
-        $document1->getTemplate()
-            ->willReturn('doc1.tpl');
-        $document1->getNumbers()
-            ->willReturn('doc01');
-        $document1->getLeft()
-            ->willReturn(99);
-        $document1->getRight()
-            ->willReturn(88);
-        $document1->getTop()
-            ->willReturn(77);
-        $document1->getBottom()
-            ->willReturn(66);
-        $document1->getPageBreak()
-            ->willReturn(55);
 
         $document2->getKey()
             ->willReturn('doc2');
-        $document2->getName()
-            ->willReturn('documentTwo');
-        $document2->getTemplate()
-            ->willReturn('doc2.tpl');
-        $document2->getNumbers()
-            ->willReturn('doc02');
-        $document2->getLeft()
-            ->willReturn(99);
-        $document2->getRight()
-            ->willReturn(88);
-        $document2->getTop()
-            ->willReturn(77);
-        $document2->getBottom()
-            ->willReturn(66);
-        $document2->getPageBreak()
-            ->willReturn(55);
-
-        $documentsRepository->findAll()
-            ->shouldBeCalled()
-            ->willReturn([$document1, $document2]);
 
         $this->dump()
             ->shouldBeLike([
@@ -125,7 +91,6 @@ class DocumentsDumperSpec extends ObjectBehavior
             ]);
     }
 
-    /*
     public function it_can_dump_document_for_older_shopware(
         ObjectRepository $documentsRepository,
         Document $document1,
@@ -135,8 +100,38 @@ class DocumentsDumperSpec extends ObjectBehavior
             return;
         }
 
-        $document1->getKey()
-            ->willReturn('doc1');
+        $this->prepareParametersForDump($documentsRepository, $document1, $document2);
+
+        $this->dump()
+            ->shouldBeLike([
+                'documentOne' => [
+                    'name'      => 'documentOne',
+                    'template'  => 'doc1.tpl',
+                    'numbers'   => 'doc01',
+                    'left'      => 99,
+                    'right'     => 88,
+                    'top'       => 77,
+                    'bottom'    => 66,
+                    'pagebreak' => 55,
+                ],
+                'documentTwo' => [
+                    'name'      => 'documentTwo',
+                    'template'  => 'doc2.tpl',
+                    'numbers'   => 'doc02',
+                    'left'      => 99,
+                    'right'     => 88,
+                    'top'       => 77,
+                    'bottom'    => 66,
+                    'pagebreak' => 55,
+                ],
+            ]);
+    }
+
+    private function prepareParametersForDump(
+        ObjectRepository $documentsRepository,
+        Document $document1,
+        Document $document2
+    ) {
         $document1->getName()
             ->willReturn('documentOne');
         $document1->getTemplate()
@@ -154,8 +149,6 @@ class DocumentsDumperSpec extends ObjectBehavior
         $document1->getPageBreak()
             ->willReturn(55);
 
-        $document2->getKey()
-            ->willReturn('doc2');
         $document2->getName()
             ->willReturn('documentTwo');
         $document2->getTemplate()
@@ -176,30 +169,5 @@ class DocumentsDumperSpec extends ObjectBehavior
         $documentsRepository->findAll()
             ->shouldBeCalled()
             ->willReturn([$document1, $document2]);
-
-        $this->dump()
-            ->shouldBeLike([
-                'doc1' => [
-                    'name'      => 'doc1',
-                    'template'  => 'doc1.tpl',
-                    'numbers'   => 'doc01',
-                    'left'      => 99,
-                    'right'     => 88,
-                    'top'       => 77,
-                    'bottom'    => 66,
-                    'pagebreak' => 55,
-                ],
-                'doc2' => [
-                    'name'      => 'doc2',
-                    'template'  => 'doc2.tpl',
-                    'numbers'   => 'doc02',
-                    'left'      => 99,
-                    'right'     => 88,
-                    'top'       => 77,
-                    'bottom'    => 66,
-                    'pagebreak' => 55,
-                ],
-            ]);
     }
-    */
 }
