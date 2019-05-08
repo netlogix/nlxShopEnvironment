d6fda1d659d212337aa2740304cf58df3da8e642
Tobias Lückel <lueckel@solutiondrive.de>
2019-05-08 17:18:10 +0200

    EO-317 Adds handling of the settings to the MediaManagerLoader

--- Loader/MediaManagerLoader.php
+++ Loader/MediaManagerLoader.php
@@ -10,6 +10,7 @@ namespace sdShopEnvironment\Loader;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Shopware\Models\Media\Album;
+use Shopware\Models\Media\Settings;
 
 class MediaManagerLoader implements LoaderInterface
 {
@@ -24,10 +25,11 @@ class MediaManagerLoader implements LoaderInterface
 
     public function load($config)
     {
-        $repository = $this->entityManager->getRepository(Album::class);
+        $albumRepository = $this->entityManager->getRepository(Album::class);
+        $settingsRepository = $this->entityManager->getRepository(Settings::class);
 
         foreach ($config as $key => $configElement) {
-            $album = $repository->find($key);
+            $album = $albumRepository->find($key);
             if (null === $album) {
                 $album = new Album();
                 $this->entityManager->persist($album);
@@ -37,10 +39,24 @@ class MediaManagerLoader implements LoaderInterface
             if (null === $parentId) {
                 $album->setParent($parentId);
             } else {
-                $album->setParent($repository->find($parentId));
+                $album->setParent($albumRepository->find($parentId));
             }
             $album->setPosition($configElement['position']);
             $album->setGarbageCollectable($configElement['garbage_collectable']);
+
+            $configSettingsElement = $configElement['settings'];
+            $settings = $settingsRepository->findOneBy(['album' => $album]);
+            if (null === $settings) {
+                $settings = new Settings();
+                $settings->setAlbum($album);
+                $this->entityManager->persist($settings);
+            }
+            $settings->setCreateThumbnails($configSettingsElement['create_thumbnails']);
+            $settings->setThumbnailSize($configSettingsElement['thumbnail_size']);
+            $settings->setIcon($configSettingsElement['icon']);
+            $settings->setThumbnailHighDpi($configSettingsElement['thumbnail_high_dpi']);
+            $settings->setThumbnailQuality($configSettingsElement['thumbnail_quality']);
+            $settings->setThumbnailHighDpiQuality($configSettingsElement['thumbnail_high_dpi_quality']);
         }
 
         $this->entityManager->flush();
--- spec/Loader/MediaManagerLoaderSpec.php
+++ spec/Loader/MediaManagerLoaderSpec.php
@@ -57,7 +57,8 @@ class MediaManagerLoaderSpec extends ObjectBehavior
 
     public function it_can_create_new_album(
         EntityManagerInterface $entityManager,
-        ObjectRepository $albumRepository
+        ObjectRepository $albumRepository,
+        ObjectRepository $settingsRepository
     ) {
         $data = [
             -1 => [
@@ -65,12 +66,25 @@ class MediaManagerLoaderSpec extends ObjectBehavior
                 'parentID' => null,
                 'position' => 13,
                 'garbage_collectable' => true,
+                'settings' => [
+                    'create_thumbnails' => 0,
+                    'thumbnail_size' => '8x8;1x1',
+                    'icon' => 'sprite-target',
+                    'thumbnail_high_dpi' => false,
+                    'thumbnail_quality' => 80,
+                    'thumbnail_high_dpi_quality' => 75,
+                ]
             ],
         ];
 
         $albumRepository->find(-1)
             ->willReturn(null);
         $entityManager->persist(Argument::type(Album::class))
+            ->shouldBeCalled();
+
+        $settingsRepository->findOneBy(Argument::any())
+            ->willReturn(null);
+        $entityManager->persist(Argument::type(Settings::class))
             ->shouldBeCalled();
 
         $entityManager->flush()
@@ -82,7 +96,9 @@ class MediaManagerLoaderSpec extends ObjectBehavior
     public function it_can_update_existing_album(
         EntityManagerInterface $entityManager,
         ObjectRepository $albumRepository,
-        Album $album
+        Album $album,
+        ObjectRepository $settingsRepository,
+        Settings $settings
     ) {
         $data = [
             -1 => [
@@ -90,6 +106,14 @@ class MediaManagerLoaderSpec extends ObjectBehavior
                 'parentID' => null,
                 'position' => 13,
                 'garbage_collectable' => true,
+                'settings' => [
+                    'create_thumbnails' => 0,
+                    'thumbnail_size' => '8x8;1x1',
+                    'icon' => 'sprite-target',
+                    'thumbnail_high_dpi' => false,
+                    'thumbnail_quality' => 80,
+                    'thumbnail_high_dpi_quality' => 75,
+                ]
             ],
         ];
 
@@ -105,6 +129,22 @@ class MediaManagerLoaderSpec extends ObjectBehavior
         $album->setPosition(13)
             ->shouldBeCalled();
         $album->setGarbageCollectable(true)
+            ->shouldBeCalled();
+
+        $settingsRepository->findOneBy(['album' => $album])
+            ->willReturn($settings);
+
+        $settings->setCreateThumbnails(0)
+            ->shouldBeCalled();
+        $settings->setThumbnailSize('8x8;1x1')
+            ->shouldBeCalled();
+        $settings->setIcon('sprite-target')
+            ->shouldBeCalled();
+        $settings->setThumbnailHighDpi(false)
+            ->shouldBeCalled();
+        $settings->setThumbnailQuality(80)
+            ->shouldBeCalled();
+        $settings->setThumbnailHighDpiQuality(75)
             ->shouldBeCalled();
 
         $entityManager->flush()
