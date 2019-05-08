0b9edd3ec65e4b221ceb952990066f618f226eee
Tobias Lückel <lueckel@solutiondrive.de>
2019-05-08 17:03:15 +0200

    EO-317 Adds step to be able to load new and existing albums

--- Loader/MediaManagerLoader.php
+++ Loader/MediaManagerLoader.php
@@ -9,6 +9,7 @@
 namespace sdShopEnvironment\Loader;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Shopware\Models\Media\Album;
 
 class MediaManagerLoader implements LoaderInterface
 {
@@ -23,8 +24,23 @@ class MediaManagerLoader implements LoaderInterface
 
     public function load($config)
     {
-        foreach ($config as $key => $configElement) {
+        $repository = $this->entityManager->getRepository(Album::class);
 
+        foreach ($config as $key => $configElement) {
+            $album = $repository->find($key);
+            if (null === $album) {
+                $album = new Album();
+                $this->entityManager->persist($album);
+            }
+            $album->setName($configElement['name']);
+            $parentId = $configElement['parentID'];
+            if (null === $parentId) {
+                $album->setParent($parentId);
+            } else {
+                $album->setParent($repository->find($parentId));
+            }
+            $album->setPosition($configElement['position']);
+            $album->setGarbageCollectable($configElement['garbage_collectable']);
         }
 
         $this->entityManager->flush();
--- spec/Loader/MediaManagerLoaderSpec.php
+++ spec/Loader/MediaManagerLoaderSpec.php
@@ -54,4 +54,62 @@ class MediaManagerLoaderSpec extends ObjectBehavior
 
         $this->load([]);
     }
+
+    public function it_can_create_new_album(
+        EntityManagerInterface $entityManager,
+        ObjectRepository $albumRepository
+    ) {
+        $data = [
+            -1 => [
+                'name' => 'Album1',
+                'parentID' => null,
+                'position' => 13,
+                'garbage_collectable' => true,
+            ],
+        ];
+
+        $albumRepository->find(-1)
+            ->willReturn(null);
+        $entityManager->persist(Argument::type(Album::class))
+            ->shouldBeCalled();
+
+        $entityManager->flush()
+            ->shouldBeCalled();
+
+        $this->load($data);
+    }
+
+    public function it_can_update_existing_album(
+        EntityManagerInterface $entityManager,
+        ObjectRepository $albumRepository,
+        Album $album
+    ) {
+        $data = [
+            -1 => [
+                'name' => 'Album1',
+                'parentID' => null,
+                'position' => 13,
+                'garbage_collectable' => true,
+            ],
+        ];
+
+        $albumRepository->find(-1)
+            ->willReturn($album);
+        $entityManager->persist(Album::class)
+            ->shouldNotBeCalled();
+
+        $album->setName('Album1')
+            ->shouldBeCalled();
+        $album->setParent(null)
+            ->shouldBeCalled();
+        $album->setPosition(13)
+            ->shouldBeCalled();
+        $album->setGarbageCollectable(true)
+            ->shouldBeCalled();
+
+        $entityManager->flush()
+            ->shouldBeCalled();
+
+        $this->load($data);
+    }
 }
