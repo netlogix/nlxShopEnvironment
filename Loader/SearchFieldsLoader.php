2b57f6a15fbf1bc3bc8e1f0cc3091a0c138f4002
Tobias Wojtylak <tw@solutiondrive.de>
2019-08-20 16:33:09 +0200

    CS-61 Save search_fields in db in SearchFieldsLoader

--- Loader/SearchFieldsLoader.php
+++ Loader/SearchFieldsLoader.php
@@ -8,13 +8,35 @@
 
 namespace sdShopEnvironment\Loader;
 
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManagerInterface;
+
 class SearchFieldsLoader implements LoaderInterface
 {
+    /** @var Connection */
+    private $connection;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->connection = $entityManager->getConnection();
+    }
+
     /**
      * {@inheritdoc}
      */
     public function load($config)
     {
-        // TODO: Implement load() method.
+        if (empty($config) || !\is_array($config)) {
+            return;
+        }
+        $sqlStatements = ['DELETE FROM `s_search_fields` WHERE 1;'];
+        foreach ($config as $searchField) {
+            $sqlStatements[] = \sprintf(
+                'INSERT INTO `s_search_fields` (%s) VALUES (%s);',
+                \implode(',', \array_keys($searchField)),
+                \implode(',', \array_map([$this->connection, 'quote'], $searchField))
+            );
+        }
+        $this->connection->exec(\implode('', $sqlStatements));
     }
 }
--- spec/Loader/SearchFieldsLoaderSpec.php
+++ spec/Loader/SearchFieldsLoaderSpec.php
@@ -8,12 +8,25 @@
 
 namespace spec\sdShopEnvironment\Loader;
 
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManagerInterface;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 use sdShopEnvironment\Loader\LoaderInterface;
 use sdShopEnvironment\Loader\SearchFieldsLoader;
 
 class SearchFieldsLoaderSpec extends ObjectBehavior
 {
+    public function let(
+        EntityManagerInterface $entityManager,
+        Connection $connection
+    ) {
+        $entityManager->getConnection()
+            ->willReturn($connection);
+
+        $this->beConstructedWith($entityManager);
+    }
+
     public function it_is_initializable()
     {
         $this->shouldHaveType(SearchFieldsLoader::class);
@@ -22,5 +35,44 @@ class SearchFieldsLoaderSpec extends ObjectBehavior
     public function it_implements_correct_interface()
     {
         $this->shouldImplement(LoaderInterface::class);
+    }
+
+    public function it_can_load_search_fields(
+        Connection $connection
+    ) {
+        $searchFields = [
+            ['id' => 1234],
+        ];
+
+        $connection->quote(1234)
+            ->shouldBeCalled()
+            ->willReturn('1234');
+
+        $connection->exec(Argument::any())
+            ->shouldBeCalled();
+
+        $this->load($searchFields);
+    }
+
+    public function it_wont_delete_existing_search_fields_on_null_argument(
+        Connection $connection
+    ) {
+        $searchFields = null;
+
+        $connection->exec(Argument::any())
+            ->shouldNotBeCalled();
+
+        $this->load($searchFields);
+    }
+
+    public function it_wont_delete_existing_search_fields_on_empty_array_as_argument(
+        Connection $connection
+    ) {
+        $searchFields = [];
+
+        $connection->exec(Argument::any())
+            ->shouldNotBeCalled();
+
+        $this->load($searchFields);
     }
 }
