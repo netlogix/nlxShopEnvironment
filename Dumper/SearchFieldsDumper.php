38529230d120e64ac543aaafeed5682257fe031f
Tobias Wojtylak <tw@solutiondrive.de>
2019-08-20 16:07:14 +0200

    CS-61 Implement SearchFieldsDumper

--- Dumper/SearchFieldsDumper.php
+++ Dumper/SearchFieldsDumper.php
@@ -8,13 +8,26 @@
 
 namespace sdShopEnvironment\Dumper;
 
+use Doctrine\ORM\EntityManagerInterface;
+
 class SearchFieldsDumper implements DumperInterface
 {
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function dump()
     {
-        // TODO: Implement dump() method.
+        $searchFields = $this->entityManager->getConnection()->executeQuery(
+            'SELECT * FROM `s_search_fields`'
+        )->fetchAll();
+        return $searchFields;
     }
 }
