4212d54ae2d9adc21d4dde4d40df13d925936546
Tobias Wojtylak <tw@solutiondrive.de>
2019-08-26 15:59:44 +0200

    CS-73 Implement PaymentRulesDumper

--- Dumper/PaymentRulesDumper.php
+++ Dumper/PaymentRulesDumper.php
@@ -8,13 +8,31 @@
 
 namespace sdShopEnvironment\Dumper;
 
+use Doctrine\Common\Persistence\ObjectRepository;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
 class PaymentRulesDumper implements DumperInterface
 {
+    /** @var ObjectRepository */
+    private $paymentRulesRepository;
+
+    /** @var NormalizerInterface */
+    private $serializer;
+
+    public function __construct(
+        ObjectRepository $paymentRulesRepository,
+        NormalizerInterface $serializer
+    ) {
+        $this->paymentRulesRepository = $paymentRulesRepository;
+        $this->serializer = $serializer;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function dump()
     {
-        // TODO: Implement dump() method.
+        $paymentRules = $this->paymentRulesRepository->findAll();
+        return $this->serializer->normalize($paymentRules);
     }
 }
--- spec/Dumper/PaymentRulesDumperSpec.php
+++ spec/Dumper/PaymentRulesDumperSpec.php
@@ -8,12 +8,22 @@
 
 namespace spec\sdShopEnvironment\Dumper;
 
+use Doctrine\Common\Persistence\ObjectRepository;
 use PhpSpec\ObjectBehavior;
 use sdShopEnvironment\Dumper\DumperInterface;
 use sdShopEnvironment\Dumper\PaymentRulesDumper;
+use Shopware\Models\Payment\RuleSet;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class PaymentRulesDumperSpec extends ObjectBehavior
 {
+    public function let(
+        ObjectRepository $paymentRulesRepository,
+        NormalizerInterface $normalizer
+    ) {
+        $this->beConstructedWith($paymentRulesRepository, $normalizer);
+    }
+
     public function it_is_initializable()
     {
         $this->shouldHaveType(PaymentRulesDumper::class);
@@ -22,5 +32,22 @@ class PaymentRulesDumperSpec extends ObjectBehavior
     public function it_implements_correct_interface()
     {
         $this->shouldImplement(DumperInterface::class);
+    }
+
+    public function it_can_dump_payment_rules(
+        ObjectRepository $paymentRulesRepository,
+        NormalizerInterface $normalizer,
+        RuleSet $ruleSet
+    ) {
+        $paymentRules = [$ruleSet];
+        $paymentRulesRepository->findAll()
+            ->willReturn($paymentRules);
+
+        $normalizer->normalize($paymentRules)
+            ->shouldBeCalled()
+            ->willReturn(['id' => 123]);
+
+        $this->dump()
+            ->shouldBe(['id' => 123]);
     }
 }
