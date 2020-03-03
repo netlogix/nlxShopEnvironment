be21a49b25a5a103eb67561eb4fb4b0ae483be96
Tobias Lückel <lueckel@solutiondrive.de>
2020-03-03 16:03:46 +0100

    Added a loader and dumper for theme settings

---
+++ Dumper/ThemeSettingsDumper.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * Created by netlogix GmbH & Co. KG
+ *
+ * @copyright netlogix GmbH & Co. KG
+ */
+
+namespace sdShopEnvironment\Dumper;
+
+use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Shopware\Models\Theme\Settings;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class ThemeSettingsDumper implements DumperInterface
+{
+    /** @var ObjectRepository */
+    private $themeSettingsRepository;
+
+    /** @var NormalizerInterface */
+    private $serializer;
+
+    public function __construct(EntityManagerInterface $entityManager, NormalizerInterface $serializer)
+    {
+        $this->themeSettingsRepository = $entityManager->getRepository(Settings::class);
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function dump()
+    {
+        $themeSettings = [];
+
+        foreach ($this->themeSettingsRepository->findAll() as $themeSetting) {
+            $themeSettings[$themeSetting->getId()] = $this->serializer->normalize($themeSetting);
+        }
+
+        return $themeSettings;
+    }
+}
---
+++ Loader/ThemeSettingsLoader.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * Created by netlogix GmbH & Co. KG
+ *
+ * @copyright netlogix GmbH & Co. KG
+ */
+
+namespace sdShopEnvironment\Loader;
+
+use Doctrine\Common\Persistence\ObjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Shopware\Models\Theme\Settings;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+class ThemeSettingsLoader implements LoaderInterface
+{
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    /** @var ObjectRepository */
+    private $themeSettingsRepository;
+
+    /** @var DenormalizerInterface */
+    private $denormalizer;
+
+    public function __construct(EntityManagerInterface $entityManager, DenormalizerInterface $denormalizer)
+    {
+        $this->entityManager = $entityManager;
+        $this->themeSettingsRepository = $entityManager->getRepository(Settings::class);
+        $this->denormalizer = $denormalizer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load($config)
+    {
+        foreach ($config as $id => $themeSettingsData) {
+            try {
+                $this->importThemeSettings($id, $themeSettingsData);
+            } catch (\Throwable $throwable) {
+                echo 'Error during import of theme setting ' . $id . PHP_EOL;
+                echo $throwable->getMessage();
+            }
+        }
+
+        $this->entityManager->flush();
+    }
+
+    /**
+     * @param string $themeSettingId
+     * @param array  $themeSettingData
+     */
+    private function importThemeSettings($themeSettingId, $themeSettingData)
+    {
+        $themeSetting = $this->themeSettingsRepository->find($themeSettingId);
+        if (null === $themeSetting) {
+            throw new \RuntimeException('Cannot import theme setting which is not already known to shopware. Because we cannot set the id');
+        }
+
+        $this->denormalizer->denormalize($themeSettingData, Settings::class, null, ['object_to_populate' => $themeSetting]);
+    }
+}
