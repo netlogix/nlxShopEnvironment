5ae98434294a356d18a2d53084b86bf16fbb2c7e
Vladislav Sultanov <vladislav.sultanov@netlogix.de>
2021-11-16 11:51:06 +0100

    Use context to ignore specific attributes instead of the method "setIgnoredAttributes" because from symfony/serializer version 5.0.0 this method not exist anymore

--- Serializer/Normalizer/PaymentRuleNormalizer.php
+++ Serializer/Normalizer/PaymentRuleNormalizer.php
@@ -10,19 +10,45 @@ namespace nlxShopEnvironment\Serializer\Normalizer;
 
 use Shopware\Models\Payment\Payment;
 use Shopware\Models\Payment\RuleSet;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
 
 class PaymentRuleNormalizer extends PropertyNormalizer
 {
+    /** @var string[] */
+    public const PAYMENT_RULE_IGNORED_ATTRIBUTES = ['paymentId'];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function denormalize($data, $type, $format = null, array $context = [])
+    {
+        $context[AbstractNormalizer::IGNORED_ATTRIBUTES] = self::PAYMENT_RULE_IGNORED_ATTRIBUTES;
+
+        return parent::denormalize($data, $type, $format, $context);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $context[AbstractNormalizer::IGNORED_ATTRIBUTES] = self::PAYMENT_RULE_IGNORED_ATTRIBUTES;
+
+        return parent::normalize($object, $format, $context);
+    }
+
     /**
      * {@inheritdoc}
      */
     public function setAttributeValue($object, $attribute, $value, $format = null, array $context = []): void
     {
+        $context[AbstractNormalizer::IGNORED_ATTRIBUTES] = self::PAYMENT_RULE_IGNORED_ATTRIBUTES;
+
         if ($this->serializer instanceof DenormalizerInterface) {
             if ('payment' === $attribute) {
-                $value = $this->serializer->denormalize($value, Payment::class, $format);
+                $value = $this->serializer->denormalize($value, Payment::class, $format, $context);
                 $object->setPaymentId($value->getId());
             }
         }
--- spec/Serializer/Normalizer/DispatchNormalizerSpec.php
+++ spec/Serializer/Normalizer/DispatchNormalizerSpec.php
@@ -14,6 +14,7 @@ use Shopware\Models\Country\Country;
 use Shopware\Models\Dispatch\Dispatch;
 use Shopware\Models\Dispatch\ShippingCost;
 use Shopware\Models\Payment\Payment;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\Serializer;
@@ -50,35 +51,41 @@ class DispatchNormalizerSpec extends ObjectBehavior
         \stdClass $object,
         Serializer $serializer
     ): void {
+        $context[AbstractNormalizer::IGNORED_ATTRIBUTES] = DispatchNormalizer::DISPATCH_PAYMENT_IGNORED_ATTRIBUTES;
+
         $serializer
-            ->denormalize([1], Country::class . '[]', null)
+            ->denormalize([1], Country::class . '[]', null, $context)
             ->shouldBeCalled()
             ->willReturn([]);
 
-        $this->setAttributeValue($object, 'countries', [1]);
+        $this->setAttributeValue($object, 'countries', [1], null, $context);
     }
 
     public function it_will_call_serializer_on_shops_attribute(
         \stdClass $object,
         Serializer $serializer
     ): void {
+        $context[AbstractNormalizer::IGNORED_ATTRIBUTES] = DispatchNormalizer::DISPATCH_PAYMENT_IGNORED_ATTRIBUTES;
+
         $serializer
-            ->denormalize([1], ShippingCost::class . '[]', null)
+            ->denormalize([1], ShippingCost::class . '[]', null, $context)
             ->shouldBeCalled()
             ->willReturn([]);
 
-        $this->setAttributeValue($object, 'costsMatrix', [1]);
+        $this->setAttributeValue($object, 'costsMatrix', [1], null, $context);
     }
 
     public function it_will_call_serializer_on_payments_attribute(
         \stdClass $object,
         Serializer $serializer
     ): void {
+        $context[AbstractNormalizer::IGNORED_ATTRIBUTES] = DispatchNormalizer::DISPATCH_PAYMENT_IGNORED_ATTRIBUTES;
+
         $serializer
-            ->denormalize([1], Payment::class . '[]', null)
+            ->denormalize([1], Payment::class . '[]', null, $context)
             ->shouldBeCalled()
             ->willReturn([]);
 
-        $this->setAttributeValue($object, 'payments', [1]);
+        $this->setAttributeValue($object, 'payments', [1], null, $context);
     }
 }
